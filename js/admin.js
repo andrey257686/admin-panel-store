93fd102f124325dbb28f90cf51ce3028251309c9
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2024-07-02 23:06:07 +0500

    feat: implement functions for pages

---
+++ js/admin.js
@@ -0,0 +1,115 @@
+if (!localStorage.getItem("products")) {
+  localStorage.setItem("products", JSON.stringify([]));
+}
+
+const PRODUCTS = JSON.parse(localStorage.getItem("products"));
+
+let USERNAME = localStorage.getItem("username");
+if (!USERNAME) {
+  USERNAME = "admin";
+  localStorage.setItem("username", USERNAME);
+}
+
+function showProducts() {
+  const productsList = document.querySelector(".product-list");
+  productsList.innerHTML = "";
+  PRODUCTS.forEach((product) => {
+    const productItem = document.createElement("div");
+    productItem.classList.add("product-list__item");
+    productItem.innerHTML = `
+      <span class="product-list__name">${product.name}</span>
+      <span class="product-list__price">${product.price} ₽</span>
+      <span class="product-list__quantity">${product.quantity}</span>
+      <span onclick="removeProduct('${product.name}')" class="product-list__remove">X</span>
+    `;
+    productsList.appendChild(productItem);
+  });
+}
+
+function saveProduct(event) {
+  event.preventDefault();
+  const productName = document.getElementById("product-name").value;
+  const productQuantity = document.getElementById("product-quantity").value;
+  const productPrice = document.getElementById("product-price").value;
+  const modalError = document.querySelector(".modal__error");
+  const product = {
+    name: productName,
+    quantity: productQuantity,
+    price: productPrice,
+  };
+  if (isProductInList(productName)) {
+    modalError.textContent = "Такой продукт уже существует";
+    modalError.style.display = "block";
+    setTimeout(() => {
+      modalError.style.display = "none";
+    }, 3000);
+  } else {
+    productName.value = "";
+    productQuantity.value = "";
+    productPrice.value = "";
+    PRODUCTS.push(product);
+    localStorage.setItem("products", JSON.stringify(PRODUCTS));
+    toggleAddProductModal(false);
+    showProducts();
+  }
+}
+
+function removeProduct(productName) {
+  PRODUCTS.forEach((product, index) => {
+    if (product.name === productName) {
+      PRODUCTS.splice(index, 1);
+      localStorage.setItem("products", JSON.stringify(PRODUCTS));
+      showProducts();
+    }
+  });
+}
+
+function isProductInList(productName) {
+  let isProductInList = false;
+  PRODUCTS.forEach((product) => {
+    if (product.name === productName) {
+      isProductInList = true;
+    }
+  });
+  return isProductInList;
+}
+
+function toggleAddProductModal(isOpen) {
+  const modal = document.getElementById("addProductModal");
+  if (isOpen) {
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
+}
+
+const addProductButton = document.getElementById("add-product");
+
+const closeModalButton = document.getElementById("close-modal");
+
+addProductButton.onclick = function () {
+  toggleAddProductModal(true);
+};
+
+closeModalButton.onclick = function () {
+  toggleAddProductModal(false);
+};
+
+const adminAvatar = document.querySelector(".header__admin-avatar");
+adminAvatar.onclick = function () {
+  const logOutButton = document.querySelector(".button--logout");
+  logOutButton.style.display = "block";
+};
+
+window.addEventListener("click", function (event) {
+  if (!event.target.matches(".header__admin-avatar")) {
+    const logOutButton = document.querySelector(".button--logout");
+    logOutButton.style.display = "none";
+  }
+});
+
+function logout() {
+  window.location.href = "index.html";
+}
+
+showProducts();
--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+if (!localStorage.getItem("users")) {
+  localStorage.setItem("users", JSON.stringify([]));
+}
+
+const USERS = JSON.parse(localStorage.getItem("users"));
+
 function toggleForms() {
   const formWrapper = document.getElementById("form-wrapper");
   formWrapper.classList.toggle("flipped");
@@ -5,45 +11,41 @@ function toggleForms() {
 
 function goToAdminPanel(event) {
   event.preventDefault();
-  const login = document.querySelector(".form__input").value;
-  const password = document.querySelector(".form__input").value;
-  if (login === "admin" && password === "admin") {
+  const loginElement = document.getElementById("login-input");
+  const passwordElement = document.getElementById("password-input");
+  // const errorLogin = document.querySelector(".form__error--login");
+  // const errorPassword = document.querySelector(".form__error--password");
+  const errorLogin = document.querySelector(".form__error");
+  const login = loginElement.value;
+  const password = passwordElement.value;
+  // let flag = true;
+  if (USERS.some((user) => user.login === login && user.password === password)) {
+    errorLogin.textContent = "";
+    loginElement.style.border = "1px solid #ccc";
+    passwordElement.style.border = "1px solid #ccc";
     window.location.href = "admin.html";
+    localStorage.setItem("username", login);
   } else {
-    alert("Неправильный логин или пароль");
+    // flag = false;
+    loginElement.style.border = "1px solid red";
+    passwordElement.style.border = "1px solid red";
+    errorLogin.textContent = "Логин или пароль неверный";
   }
+  // if (flag) {
+  //   window.location.href = "admin.html";
+  // }
 }
 
-function toggleAddProductModal(isOpen) {
-  const modal = document.getElementById("addProductModal");
-  if (isOpen) {
-    modal.style.display = "block";
-  } else {
-    modal.style.display = "none";
-  }
+function registerUser(event) {
+  event.preventDefault();
+  const loginElement = document.getElementById("register-login-input");
+  const passwordElement = document.getElementById("register-password-input");
+  const login = loginElement.value;
+  const password = passwordElement.value;
+  USERS.push({
+    login,
+    password,
+  });
+  localStorage.setItem("users", JSON.stringify(USERS));
+  toggleForms();
 }
-
-const addProductButton = document.getElementById("add-product");
-
-const closeModalButton = document.getElementById("close-modal");
-
-addProductButton.onclick = function () {
-  toggleAddProductModal(true);
-};
-
-closeModalButton.onclick = function () {
-  toggleAddProductModal(false);
-};
-
-const adminAvatar = document.querySelector(".header__admin-avatar");
-adminAvatar.onclick = function () {
-  const logOutButton = document.querySelector(".button--logout");
-  logOutButton.style.display = "block";
-};
-
-window.addEventListener("click", function (event) {
-  if (!event.target.matches(".header__admin-avatar")) {
-    const logOutButton = document.querySelector(".button--logout");
-    logOutButton.style.display = "none";
-  }
-});
